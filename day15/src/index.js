8902823a2968523d95785d50cb8ade3c3251aaeb
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2021-07-08 21:35:50 +0530

    Add files via upload

---
+++ day15/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import Meme from './meme';
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Meme />, document.getElementById("root"));
